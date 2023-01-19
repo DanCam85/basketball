1367e3bccc216283e3e996f88f08efe19ea77994
DanCam85 <96871656+DanCam85@users.noreply.github.com>
2023-01-19 20:43:13 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
+let homeCount = 0;
+let guestCount = 0;
+
+function homeAddOne() {
+  homeCount += 1;
+  homeScore.textContent = homeCount;
+}
+
+function homeAddTwo() {
+  homeCount += 2;
+  homeScore.textContent = homeCount;
+}
+
+function homeAddThree() {
+  homeCount += 3;
+  homeScore.textContent = homeCount;
+}
+
+function guestAddOne() {
+  guestCount += 1;
+  guestScore.textContent = guestCount;
+}
+
+function guestAddTwo() {
+  guestCount += 2;
+  guestScore.textContent = guestCount;
+}
+
+function guestAddThree() {
+  guestCount += 3;
+  guestScore.textContent = guestCount;
+}
+
+function endGame() {
+  guestCount = 0;
+  homeCount = 0;
+  guestScore.textContent = guestCount;
+  homeScore.textContent = homeCount;
+}
